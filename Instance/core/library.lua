4be41d3409b04250af88ccba65ca68af265f7e1a
Afterpartyghost <priyanshgupta1902@gmail.com>
2026-06-25 23:51:08 +0530

    Refactor Library: Move repo loading outside Init
    Removed redundant loading of library components from Init function.

--- Instance/core/library.lua
+++ Instance/core/library.lua
@@ -1,17 +1,13 @@
--- core/library.lua - Obsidian Library
+-- core/library.lua
+local repo = "https://raw.githubusercontent.com/deividcomsono/Obsidian/main/"
+
+local lib = loadstring(game:HttpGet(repo .. "Library.lua"))()
+local theme = loadstring(game:HttpGet(repo .. "addons/ThemeManager.lua"))()
+local save = loadstring(game:HttpGet(repo .. "addons/SaveManager.lua"))()
+
 local Library = {}
 
 function Library:Init()
-    local repo = "https://raw.githubusercontent.com/deividcomsono/Obsidian/main/"
-    
-    local lib = loadstring(game:HttpGet(repo .. "Library.lua"))()
-    local theme = loadstring(game:HttpGet(repo .. "addons/ThemeManager.lua"))()
-    local save = loadstring(game:HttpGet(repo .. "addons/SaveManager.lua"))()
-    
-    if not lib then
-        error("Failed to load Obsidian Library")
-    end
-    
     return lib, theme, save
 end
 
